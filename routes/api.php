ad7a771cf85bbff98894ec3455f838ce733cd612
Miguel-Restrepo <miguel.1701911153@ucaldas.edu.co>
2023-10-01 21:57:09 -0500

    api routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\RoomController;
+use App\Http\Controllers\TimeBussyController;
+use App\Http\Controllers\UserController;
+use App\Models\RequestRoom;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +18,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::controller(RoomController::class)->group(function () {
+    Route::get('room', 'index');
+    Route::get('room/{id}', 'show');
+    Route::post('room', 'store');
+    Route::put('room/{id}', 'update');
+    Route::delete('room/{id}/delete', 'destroy');
+});
+
+Route::controller(RequestRoom::class)->group(function () {
+    Route::get('request-room', 'index');
+    Route::get('request-room/{id}', 'show');
+    Route::post('request-room', 'store');
+    Route::put('request-room/{id}', 'update');
+    Route::delete('request-room/{id}/delete', 'destroy');
+});
+
+Route::controller(TimeBussyController::class)->group(function () {
+    Route::get('time-bussy', 'index');
+    Route::get('time-bussy/{id}', 'show');
+    Route::post('time-bussy', 'store');
+    Route::put('time-bussy/{id}', 'update');
+    Route::delete('time-bussy/{id}/delete', 'destroy');
+});
+
+Route::controller(UserController::class)->group(function () {
+    Route::get('user', 'index');
+    Route::get('user/{id}', 'show');
+    Route::post('user', 'store');
+    Route::put('user/{id}', 'update');
+    Route::delete('user/{id}/delete', 'destroy');
 });
