dd10c2f4b91d5d1303c0b871450dbbe75d3098c6
Miguel-Restrepo <miguel.1701911153@ucaldas.edu.co>
2023-10-09 20:56:41 -0500

    header and footer Update

--- coordisalas-app-front/src/app/public/master-page/header/header.component.ts
+++ coordisalas-app-front/src/app/public/master-page/header/header.component.ts
@@ -5,5 +5,16 @@ import { Component } from '@angular/core';
   templateUrl: './templates/header.component.html',
 })
 export class HeaderComponent {
+  date: string = '';
+  
+  ngOnInit(){
+    this.internalInit();
+  }
 
+  internalInit(){
+    const today = new Date();
+    const days = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
+    const day = days[today.getDay()];
+    this.date = `${day} ${today.getDate()}/${today.getMonth() + 1}/${today.getFullYear()}`;
+  }
 }
