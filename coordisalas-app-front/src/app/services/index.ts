db904da54efc5242ea580584f3fc6d3176e05dc7
Miguel-Restrepo <miguel.1701911153@ucaldas.edu.co>
2023-10-15 15:10:43 -0500

    Add services

--- coordisalas-app-front/src/app/services/index.ts
+++ coordisalas-app-front/src/app/services/index.ts
@@ -1,1 +1,5 @@
+export * from './Request/request.service';
+export * from './Room/room.service';
 export * from './Security/security.service';
+export * from './TimeBussy/time-bussy.service';
+export * from './User/user.service';
